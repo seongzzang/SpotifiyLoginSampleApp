70b08ab9b5b705e0465d53a27dc694aeac1d643c
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-08-27 20:03:47 +0900

    update_mainView

---
+++ SpotifiyLoginSampleApp/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  SpotifiyLoginSampleApp
+//
+//  Created by 양성혜 on 2022/08/27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+   
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+}
