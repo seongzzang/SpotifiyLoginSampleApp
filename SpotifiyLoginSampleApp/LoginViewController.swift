4f1b40251c8876f37a6f06ea62ce24449519977b
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-08-27 19:25:29 +0900

    update_setting3

--- SpotifiyLoginSampleApp/LoginViewController.swift
+++ SpotifiyLoginSampleApp/LoginViewController.swift
@@ -9,10 +9,33 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    
+    @IBOutlet weak var emailLoginButton: UIButton!
+    
+    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        [emailLoginButton,googleLoginButton,appleLoginButton].forEach{
+            $0?.layer.borderWidth = 1
+            $0?.layer.borderColor = UIColor.white.cgColor
+            $0?.layer.cornerRadius = 30
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // navigationbar 숨기기
         navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func googleLoginButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func appleLoginButtonTapped(_ sender: UIButton) {
     }
 }
